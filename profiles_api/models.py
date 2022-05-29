f5aa822e27e4a960318c2e3c11296a6a44809f0e
Zack <zib5026@gmail.com>
2022-05-29 15:51:31 -0400

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -16,7 +16,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(usering=self._db)
+        user.save(using=self._db)
 
         return user
 
